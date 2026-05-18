8e58e5994320c46b94688158793af9a77783be87
ocealis <ixneo.discord@gmail.com>
2026-05-18 11:47:26 +0200

    Amélioration de la gestion du PCF8574

--- include/sensors/i2c/pcf8574.h
+++ include/sensors/i2c/pcf8574.h
@@ -4,8 +4,8 @@
 #include <Wire.h>
 #include <Arduino.h>
 
-void initPCF8574(uint8_t adresse);
-void readPCF8574(uint8_t adresse, uint8_t &valeur);
-void afficheLes8bitsDeLaValeur(uint8_t valeur);
+bool initPCF8574(uint8_t addr);
+bool readPCF8574(uint8_t addr, uint8_t &value);
+void afficheLes8bitsDeLaValeur(uint8_t value);
 
 #endif
--- src/sensors/i2c/pcf8574.cpp
+++ src/sensors/i2c/pcf8574.cpp
@@ -1,10 +1,29 @@
 #ifndef PCF8574_H
 #define PCF8574_H
 
+#include <Arduino.h>
+#include "sensors/i2c/pcf8574.h"
 #include <Wire.h>
 
-void initPCF8574(uint8_t adresse);
-void readPCF8574(uint8_t adresse, uint8_t &valeur);
-void afficheLes8bitsDeLaValeur(uint8_t valeur);
+bool initPCF8574(uint8_t adresse) {
+    Wire.beginTransmission(adresse);
+    return Wire.endTransmission() == 0;
+}
+
+bool readPCF8574(uint8_t adresse, uint8_t &valeur) {
+    Wire.requestFrom(adresse, (uint8_t)1);
+    if (Wire.available()) {
+        valeur = Wire.read();
+        return true;
+    }
+    return false;
+}
+
+void afficheLes8bitsDeLaValeur(uint8_t valeur) {
+    for (int i = 7; i >= 0; i--) {
+        Serial.print((valeur >> i) & 1);
+    }
+    Serial.println();
+}
 
 #endif
